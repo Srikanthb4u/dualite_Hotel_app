b59aafdd338ac2d6546504b04421f18fb754b772
Srikanthb4u <104753105+Srikanthb4u@users.noreply.github.com>
2025-09-04 07:39:20 +0530

    Update firebaseConfig.ts

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyCbFA0Tkeb8Wzh3Kxms0bxP_WDxfrl5OTo",
+  authDomain: "inn-craft.firebaseapp.com",
+  projectId: "inn-craft",
+  storageBucket: "inn-craft.firebasestorage.app",
+  messagingSenderId: "914444514349",
+  appId: "1:914444514349:web:cfd7299f1c72e485e374a4",
+  measurementId: "G-B795TJL6ND"
 };
 
 // Initialize Firebase
